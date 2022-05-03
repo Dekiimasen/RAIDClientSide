c42d9399343359b9f41538d0ec9d51e2bce19ceb
Amos Goh <amos.goh@websparks.sg>
2022-05-03 23:14:43 +0800

    RAiD Project.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, AppBar, Typography, Grow, Grid} from '@mui/material';
+import UserInput from './components/showuserinput/ShowUserInput.js';
+import CreateUserInput from './components/createuserinput/CreateUserInput.js';
+import useStyles from './styles'
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxwidth="lg">
+          <Typography className={classes.heading} variant='h2' align='center'>RAiD App</Typography>
+
+          <Container>
+            <Grid container justify='space-between'>
+              
+              <Grid item xs={12} sm={7}>
+                  <UserInput />
+              </Grid>
+
+              <Grid item xs={12} sm={4}>
+                <CreateUserInput />
+              </Grid>
+
+            </Grid>
+          </Container>
+      </Container>
     </div>
   );
 }
---
+++ src/components/showuserinput/ShowUserInput.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import { textAlign } from '@mui/system';
+import axios from 'axios';
+import { Paper } from '@mui/material';
+import { Table,TableBody,TableContainer,TableHead,TableRow,TableCell } from '@mui/material';
+
+const columns = [
+  { field: 'UserInput', headerName: 'User Input', width: 200 },
+  { field: 'UserName', headerName: 'User Name', width: 130 },
+];
+
+const rows = [
+  {UserInput: ShowUserInput.UserInput, UserName: ShowUserInput.UserName}
+];
+
+export default function ShowUserInput() {
+  const [UserInputList, setUserInputList] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:5000/UserInputs').then( (allUserInputs) => {
+      setUserInputList(allUserInputs.data);
+    })
+  }, [])
+  return (
+      <><h2>User Inputs</h2>
+    <div style={{ height: 400, width: '100%'}}>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>User Input</TableCell>
+              <TableCell align='right'>User Name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {UserInputList.map((UserInput,key) => (
+              <TableRow key={key}>
+                <TableCell component="th" scope="row">
+                  {UserInput.Input}
+                </TableCell>
+                <TableCell align='right'>{UserInput.Name}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        </TableContainer>
+    </div>
+    </>
+  );
+}
